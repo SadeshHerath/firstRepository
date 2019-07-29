98cfe783c84d057ddae6b363d24d688cb10edd73
student <anuradaherathkv2gmail.com>
2019-07-29 16:49:06 +0530

    Commit msg goes here...

---
+++ app/src/main/java/com/example/student/mylab2/MainActivityGIT.java
@@ -0,0 +1,13 @@
+package com.example.student.mylab2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivityGIT extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_git);
+    }
+}
